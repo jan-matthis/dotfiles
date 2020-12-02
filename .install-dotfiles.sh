1e83afffc8070af0883505b98d6050d3a7dd24bc
jan-matthis <mail@jan-matthis.de>
2020-12-02 14:48:25 +0100

    Update .install-dotfiles.sh

--- .install-dotfiles.sh
+++ .install-dotfiles.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-git clone --bare git@github.com:jan-matthis/dotfiles.git $HOME/.cfg
+set -o pipefail
+shopt -s expand_aliases
+
 alias cfg='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
-# Checkout can fail if files exist, move offending files to backup folder
-mkdir -p .config-backup
-cfg checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} dirname {} | awk {'print $1'} | xargs -I{} mkdir -p .config-backup/{}
-cfg checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-
-cfg checkout
-cfg config --local status.showUntrackedFiles no
-
-echo "Successfully installed dotfiles"
+if [ -d "$HOME/.cfg" ]
+then
+  cfg pull
+  echo "Updated dotfiles"
+else
+  git clone --bare git@github.com:jan-matthis/dotfiles.git $HOME/.cfg
+  mkdir -p .config-backup
+  cfg checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} dirname {} | awk {'print $1'} | xargs -I{} mkdir -p .config-backup/{}
+  cfg checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+  cfg checkout
+  cfg config --local status.showUntrackedFiles no
+  echo "Installed dotfiles"
+fi
